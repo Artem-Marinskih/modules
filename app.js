7136b941c6fef73c691d56b1e726dce015b08e71
Artem_Marinskih <a.marinskih1@gmail.com>
2021-10-21 23:42:42 +0500

    fixed

--- app.js
+++ app.js
@@ -12,20 +12,34 @@ i.classList.add("fa", "fa-pencil");
 app.append(i);
 
 function getMonth(dateResult) {
-  let month = dateResult.getMonth();
+  const month = dateResult.getMonth();
   switch (month) {
-    case 0: return 'января';
-    case 1: return 'февраля';
-    case 2: return 'марта';
-    case 3: return 'апреля';
-    case 4: return 'мая';
-    case 5: return 'июня';
-    case 6: return 'июля';
-    case 7: return 'августа';
-    case 8: return 'сентября';
-    case 9: return 'октября';
-    case 10: return 'ноября';
-    case 11: return 'декабря';
+    case 0:
+      return "января";
+    case 1:
+      return "февраля";
+    case 2:
+      return "марта";
+    case 3:
+      return "апреля";
+    case 4:
+      return "мая";
+    case 5:
+      return "июня";
+    case 6:
+      return "июля";
+    case 7:
+      return "августа";
+    case 8:
+      return "сентября";
+    case 9:
+      return "октября";
+    case 10:
+      return "ноября";
+    case 11:
+      return "декабря";
+    default:
+      return "месяц";
   }
 }
 
@@ -36,7 +50,7 @@ pencil.addEventListener("click", () => {
     props: {
       closable: true,
       closepos: "modal-closer-right",
-      bgclose: false,
+      bgclose: true,
       content: `<input class='date' value = ${noDate.textContent}>`,
       heading: "Выберете дату и время",
       buttons: [
@@ -53,22 +67,20 @@ pencil.addEventListener("click", () => {
     let date = datePicker.selectedDates[0];
     let modal = document.querySelector(".modal-inner");
 
-    if (date === undefined) {
-      modal.classList.add("wibro");
+    if (!date) {
+      modal.classList.add("vibro");
       vanillaToast.show("Ошибка", { className: "error" });
 
-    setTimeout( ()=>{
-        modal.classList.remove("wibro");
-    }, 1000)
+      setTimeout(() => {
+        modal.classList.remove("vibro");
+      }, 1000);
       return;
     }
 
     const dateResult = new Date(date);
-    noDate.textContent =
-      dateResult.getDate() + " " +
-      getMonth(dateResult) + " " +
-      dateResult.getFullYear() + ' ' +
-      dateResult.getHours() + ":" + dateResult.getMinutes() + " ";
+    noDate.textContent = `${dateResult.getDate()} ${getMonth(
+      dateResult
+    )} ${dateResult.getFullYear()} ${dateResult.getHours()}: ${dateResult.getMinutes()} `;
     myModal.$set({ visible: false });
   });
 
@@ -82,4 +94,3 @@ pencil.addEventListener("click", () => {
     locale: "ru",
   });
 });
-
